3fa69316b9bcfca14e6e685669ad7f457277795e
Sarah Giapitzakis <espeonblumbreon@gmail.com>
2016-06-27 19:59:01 +1000

    set up the skeleton of the APIHandler methods
    Set up the skeleton of the APIHandler (mostly empty) methods for
    fetching and parsing the JSON ticket data

--- src/com/zendesk/main/Main.java
+++ src/com/zendesk/main/Main.java
@@ -1,5 +1,16 @@
 package com.zendesk.main;
 
 public class Main {
+	
+	public static void main(String[] args) {
+		
+		//initialize connection to Zendesk
+		
+		//check connection was successful
+		
+		//display menu
+		
+		return;
+	}
 
 }
--- src/com/zendesk/model/APIHandler.java
+++ src/com/zendesk/model/APIHandler.java
@@ -1,5 +1,39 @@
 package com.zendesk.model;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+
+
 public class APIHandler {
 
+	//Get the tickets
+	public JSONObject getTickets(){
+		//connect to API and get the tickets in JSON format
+		JSONObject ticketsJSON = new JSONObject();
+		ticketsJSON = connectToAPI();
+		
+		//format the tickets into neater JSON
+		ticketsJSON = formatJSON(ticketsJSON);
+		
+		//return the formatted tickets
+		return ticketsJSON;
+	}
+	
+	//connect to the API and handle API issues
+	public JSONObject connectToAPI(){
+		//connect to the API
+		JSONObject ticketsJSON = new JSONObject();
+		//doesn't actually do anything yet but it will!
+		
+		return ticketsJSON;
+	}
+	
+	//format the JSON
+	public JSONObject formatJSON(JSONObject ticketsJSON){
+		//format the tickets into a display-able format
+		//doesn't actually do anything yet but it will!
+		return ticketsJSON;
+	}
+	
 }
