50ed7c7ae9a9357bd2d4894ff7fc686063016e8e
Sarah Giapitzakis <espeonblumbreon@gmail.com>
2016-07-01 14:37:59 +1000

    populating main view with CL user interface
    Populating the MainView with user interface text and functions for
    displaying text

--- src/com/zendesk/view/MainView.java
+++ src/com/zendesk/view/MainView.java
@@ -1,5 +1,39 @@
 package com.zendesk.view;
 
+import java.util.Scanner;
+
 public class MainView {
 
+	String[] menuOptions = {"Display All Tickets","Display A Single Ticket","Exit Program"};
+	String welcomeMsg = "--- Hello and welcome to the Zendesk ticket viewer! ---";
+	String menuInstructions = "Please type a number to select a menu option (or press q to quit):";
+	String confirmExit = "Are you sure you wish exit the program? (y / n):";
+	
+	Scanner sc;
+	public void printMainMenu(){
+		printMenuInstructions();
+		printMenuOptions();
+		return;
+	}
+	
+	public void printMenuInstructions(){
+		System.out.println(menuInstructions);
+		return;
+	}
+	
+	public void printWelcome(){
+		System.out.println(welcomeMsg);
+		return;
+	}
+	
+	public void printMenuOptions(){
+		System.out.println("---------------------------------");
+		for(int i = 1; i <= menuOptions.length; i++){
+			System.out.println(i + ": "+ menuOptions[i-1]);
+		}
+		System.out.println("---------------------------------");
+		
+	}
+
+	
 }
