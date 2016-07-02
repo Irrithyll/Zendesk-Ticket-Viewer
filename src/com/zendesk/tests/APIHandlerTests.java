d628f932d4c1c03b68e84f8e3df7797c9dbff424
Sarah Giapitzakis <espeonblumbreon@gmail.com>
2016-07-02 22:48:04 +1000

    Setup jUnit imports for APIHandler tests
    Setup jUnit imports for APIHandler tests

--- src/com/zendesk/tests/APIHandlerTests.java
+++ src/com/zendesk/tests/APIHandlerTests.java
@@ -1,5 +1,15 @@
 package com.zendesk.tests;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class APIHandlerTests {
 
+	@Test
+	public void testAPIOfflineExeption(){
+		
+		assertEquals(true,true);
+	}
+	
 }
