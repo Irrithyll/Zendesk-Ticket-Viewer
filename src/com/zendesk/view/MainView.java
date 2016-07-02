324a1a44aecb5a85fa1ef81846409c6ad0ced89a
Sarah Giapitzakis <espeonblumbreon@gmail.com>
2016-07-02 22:47:19 +1000

    Added user interface text and functions to MainView
    Populated the MainView class with reusable user interface text and
    functions

--- src/com/zendesk/view/MainView.java
+++ src/com/zendesk/view/MainView.java
@@ -1,18 +1,21 @@
 package com.zendesk.view;
 
-import java.util.Scanner;
 
 public class MainView {
 
-	String[] menuOptions = {"Display All Tickets","Display A Single Ticket","Exit Program"};
-	String welcomeMsg = "--- Hello and welcome to the Zendesk ticket viewer! ---";
-	String menuInstructions = "Please type a number to select a menu option (or press q to quit):";
+	String[] menuOptions = {"Display All Tickets","Display A Single Ticket","Quit Program"};
+	String welcomeMsg = "Hello and welcome to the Zendesk ticket viewer!";
+	String welcomeInstructions = "Please type menu to view the menu, or q to quit the program:";
+	String menuHeader = "---------------------------------\nMenu\n---------------------------------";
+	String menuInstructions = "Please type a number to select a menu option (or type q to quit):";
 	String confirmExit = "Are you sure you wish exit the program? (y / n):";
-	
-	Scanner sc;
+	String quitMsg = "Program Exiting...";
+	String unrecognizedInputErr = "Error: Unrecognized input, please enter your selection again:";
+
 	public void printMainMenu(){
-		printMenuInstructions();
+		printMenuHeader();
 		printMenuOptions();
+		printMenuInstructions();
 		return;
 	}
 	
@@ -23,11 +26,24 @@ public class MainView {
 	
 	public void printWelcome(){
 		System.out.println(welcomeMsg);
+		System.out.println(welcomeInstructions);
 		return;
 	}
 	
+	public void printMenuHeader(){
+		System.out.println(menuHeader);
+		return;
+	}
+	
+	public void quit(){
+		System.out.println(quitMsg);
+	}
+	
+	public void unrecognizedInput(){
+		System.out.println(unrecognizedInputErr);
+	}
+	
 	public void printMenuOptions(){
-		System.out.println("---------------------------------");
 		for(int i = 1; i <= menuOptions.length; i++){
 			System.out.println(i + ": "+ menuOptions[i-1]);
 		}
